ffb6a1a60636751229ef46254e10deedfbb51ccd
miguel5612 <miguelangelcu@ufps.edu.co>
2018-01-23 21:34:02 -0500

    se corrige un problema detectado al tener la direccion del eje Y invertido

--- firmware/Marlin-1.1.x/Marlin/Configuration.h
+++ firmware/Marlin-1.1.x/Marlin/Configuration.h
@@ -749,7 +749,7 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
-#define INVERT_Y_DIR true
+#define INVERT_Y_DIR false
 #define INVERT_Z_DIR false
 
 // Enable this option for Toshiba stepper drivers
