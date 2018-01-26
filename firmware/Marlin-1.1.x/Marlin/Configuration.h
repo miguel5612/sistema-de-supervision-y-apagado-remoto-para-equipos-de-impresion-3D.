35d4caf172f3e05e0859fae075ad185e9b3b0a09
miguel5612 <miguelangelcu@ufps.edu.co>
2018-01-25 21:06:13 -0500

    se configuran exitosamente los dos termistores HotEnd - HotBed  y se realiza prueba de calentamiento del extrusor

--- firmware/Marlin-1.1.x/Marlin/Configuration.h
+++ firmware/Marlin-1.1.x/Marlin/Configuration.h
@@ -291,7 +291,7 @@
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
 #define TEMP_SENSOR_4 0
-#define TEMP_SENSOR_BED 0
+#define TEMP_SENSOR_BED 80
 
 // Dummy thermistor constant temperature readings, for use with 998 and 999
 #define DUMMY_THERMISTOR_998_VALUE 25
