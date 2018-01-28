550177412aa1225f7eeb85035cbdd2ed1bfe9626
miguel5612 <miguelangelcu@ufps.edu.co>
2018-01-28 13:47:57 -0500

    ajustes PID

--- firmware/Marlin-1.1.x/Marlin/Configuration.h
+++ firmware/Marlin-1.1.x/Marlin/Configuration.h
@@ -355,9 +355,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define  DEFAULT_Kp 22.2
-  #define  DEFAULT_Ki 1.08
-  #define  DEFAULT_Kd 114
+  //#define  DEFAULT_Kp 22.2
+  //#define  DEFAULT_Ki 1.08
+  //#define  DEFAULT_Kd 114
 
   // MakerGear
   //#define  DEFAULT_Kp 7.0
@@ -365,9 +365,9 @@
   //#define  DEFAULT_Kd 12
 
   // Mendel Parts V9 on 12V
-  //#define  DEFAULT_Kp 63.0
-  //#define  DEFAULT_Ki 2.25
-  //#define  DEFAULT_Kd 440
+  #define  DEFAULT_Kp 63.0
+  #define  DEFAULT_Ki 2.25
+  #define  DEFAULT_Kd 440
 
 #endif // PIDTEMP
 
