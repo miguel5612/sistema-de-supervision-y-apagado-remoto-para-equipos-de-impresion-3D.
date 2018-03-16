38279127272e87f4a0346eee1027ea706017dbc8
Miguel angel Califa <Miguel angel Califa>
2018-03-15 22:55:26 -0500

    Se realizan las correcciones pertinentes para una lectura mas precisa de la temperatura

--- Software/Node MCU/micropython/DHT11 to THINGSPEAK/main.py
+++ Software/Node MCU/micropython/DHT11 to THINGSPEAK/main.py
@@ -11,7 +11,7 @@ d = dht.DHT11(machine.Pin(DHT_PIN))
 def main(use_stream=True):
     while True:
         d.measure()
-        data = b"api_key="+ API_KEY + "&field1=" + str(d.temperature()) + "&field2=" + str(d.humidity())
+        data = b"api_key="+ API_KEY + "&field1=" + str(d.temperature()-4) + "&field2=" + str(d.humidity())
         s = _socket.socket()
         ai = _socket.getaddrinfo(HOST, 443)
         addr = ai[0][-1]
@@ -24,4 +24,18 @@ def main(use_stream=True):
         print(s.read(128))
         s.close()
         time.sleep(60)
+def test(use_stream=True):
+    d.measure()
+    data = b"api_key="+ API_KEY + "&field1=" + str(d.temperature()) + "&field2=" + str(d.humidity())
+    s = _socket.socket()
+    ai = _socket.getaddrinfo(HOST, 443)
+    addr = ai[0][-1]
+    s.connect(addr)
+    s = ssl.wrap_socket(s)  
+    s.write("POST /update HTTP/1.0\r\n")
+    s.write("Host: " + HOST + "\r\n")
+    s.write("Content-Length: " + str(len(data)) + "\r\n\r\n")
+    s.write(data)
+    print(s.read(128))
+    s.close()
 main()
