010e7163d71a7731597647587e61601948276c2b
Miguel angel Califa <Miguel angel Califa>
2018-03-19 18:26:41 -0500

    Se realiza la correccion del loop main para que no se detenga el while True

--- Software/Node MCU/micropython/DHT11 to THINGSPEAK/main.py
+++ Software/Node MCU/micropython/DHT11 to THINGSPEAK/main.py
@@ -4,21 +4,24 @@ import time
 print("main invocado")
 time.sleep(10)
 print("main en ejecucion.")
-while(True):
-	print("while true")
-	wlan = wifimgr.get_connection()
+try:
+	while(True):
+		print("while true")
+		wlan = wifimgr.get_connection()
 
-	if wlan is None:
+		if wlan is None:
 
-		print("Could not initialize the network connection.")
+			print("Could not initialize the network connection.")
 
-	try:
-		print("Tomando muestra y publicando")
-		publishThingspeak.calculateAndPublish()
-		print("publicado")
-		time.sleep(10)
-		print("paso el tiempo.")
+		try:
+			print("Tomando muestra y publicando")
+			publishThingspeak.calculateAndPublish()
+			print("publicado")
+			time.sleep(10)
+			print("paso el tiempo.")
 
-	finally:
-		import boardReset
-		boardReset.confirmandreset()
+		except:
+			wlan = wifimgr.get_connection()
+except:
+	print("Fallo el while True, Reintentando...")
+	main()
